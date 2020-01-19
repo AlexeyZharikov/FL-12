0ad3a149e75e46934f982bc299230cfbdd12212a
Oleksii Zharikov <mefis2.0@gmail.com>
2020-01-19 13:02:14 +0200

    second

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -16,6 +16,7 @@ function Fighter({
     this.addWin = () => {
         return this.wins++
     }
+
     this.addLoss = () => {
         return this.losses++;
     }
@@ -23,18 +24,23 @@ function Fighter({
     this.getName = () => {
         return this.name;
     }
+
     this.getDamage = () => {
         return this.damage;
     }
+
     this.getHealth = () => {
         return this.hp;
     }
+
     this.getStrength = () => {
         return this.strength;
     }
+
     this.getAgility = () => {
         return this.agility;
     }
+
     this.dealDamage = (num) => {
         let currentHealth = this.getHealth() - num;
         this.hp = currentHealth;
@@ -44,5 +50,44 @@ function Fighter({
         }
         return currentHealth;
     }
-//here
+
+    this.heal = (num) => {
+        let currentHealth = this.getHealth() + num;
+        this.hp = currentHealth;
+        return currentHealth;
+    }
+
+    this.logCombatHistory = () => {
+
+        return 'Name: ' + this.name + ' ' + ' Wins: ' + this.wins + ' ' + ' Losses: ' + this.losses;
+
+    }
+
+
+    this.attack = (Fighter) => {
+        let attackProb = (100 - (Fighter.getStrength() + Fighter.getAgility())) / 100;
+        if (attackProb >= 0 && attackProb <= 0.5) {
+            if (Math.floor(Math.random() * 2) > 0) {
+                console.log(this.name + ' makes ' + this.damage + ' to ' + Fighter.getName());
+                return Fighter.dealDamage(this.getDamage());
+            } else {
+                console.log(this.name + ' attack missed');
+                return Fighter.dealDamage(0);
+            }
+        } else if (attack > 0.5 && attack <= 0.8) {
+            if (Math.floor(Math.random() * 4) > 0) {
+                console.log(this.name + ' makes ' + this.damage + ' to ' + Fighter.getName());
+                return Fighter.dealDamage(this.getDamage());
+            } else {
+                return Fighter.getHealth();
+            }
+        } else if (attack > 0.8 && attack <= 1) {
+            if (Math.floor(Math.random() * 8) > 0) {
+                console.log(this.name + ' makes ' + this.damage + ' to ' + Fighter.getName());
+                return Fighter.dealDamage(this.getDamage());
+            } else {
+                return Fighter.getHealth();
+            }
+        };
+    }
 }
